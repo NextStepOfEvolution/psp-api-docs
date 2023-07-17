8a568f75ce18703e1d7b6ef42dd32383d34cb104
ibrokhim <ibo-707@mail.ru>
2023-07-17 15:35:58 +0500

    fix

--- src/views/GettingStarted.vue
+++ src/views/GettingStarted.vue
@@ -4,18 +4,18 @@
 <template>
     <div class="">
         <h1 class="text-4xl font-bold">Начало работы</h1>
-        <p>Мы подготовили инструкции по интеграции, чтобы сделать процесс подключения к PSP, а также прием платежей, быстрым и легким.</p>
+        <p class="my-4">Мы подготовили инструкции по интеграции, чтобы сделать процесс подключения к PSP, а также прием платежей, быстрым и легким.</p>
         <div class="grid grid-cols-2 gap-4">
-            <Card class="text-center">
+            <Card class="text-center dark:text-gray-200 dark:bg-gray-900">
               <template #header>
                   <img class="mx-auto" src="@/assets/images/tools/terminal.png" alt="Web-касса"/>
               </template>
-              <template #title><b>Web-касса</b></template> 
+              <template #title><b><router-link :to="{name: 'webkassa'}">Web-касса</router-link></b></template> 
               <template #content>
                 <p>Удобный способ принимать платежи.</p> 
               </template> 
             </Card> 
-            <Card class="text-center">
+            <Card class="text-center dark:text-gray-200 dark:bg-gray-900">
               <template #header>
                   <img class="mx-auto" src="@/assets/images/tools/terminal.png" alt="Web-касса"/>
               </template>
@@ -24,7 +24,7 @@
                 <p>Легкий и быстрый способ начать принимать платежи от пользователей</p> 
               </template> 
             </Card> 
-            <Card class="text-center">
+            <Card class="text-center dark:text-gray-200 dark:bg-gray-900">
               <template #header>
                   <img class="mx-auto" src="@/assets/images/tools/terminal.png" alt="Web-касса"/>
               </template>
@@ -33,7 +33,7 @@
                 <p>Легкий и быстрый способ начать принимать платежи от пользователей</p> 
               </template> 
             </Card> 
-            <Card class="text-center">
+            <Card class="text-center dark:text-gray-200 dark:bg-gray-900">
               <template #header>
                   <img class="mx-auto" src="@/assets/images/tools/terminal.png" alt="Web-касса"/>
               </template>
