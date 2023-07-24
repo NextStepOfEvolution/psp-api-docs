1c120fad24fc3fec2f5e591a634700e5e1e06d0d
ibrokhim <ibo-707@mail.ru>
2023-07-24 17:56:12 +0500

    build

---
+++ dist/assets/ProccessPage-c24f30d9.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as c,c as t,d as n}from"./index-ff2b9fe3.js";const s={};function a(_,l){const e=r("router-view");return c(),t("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
