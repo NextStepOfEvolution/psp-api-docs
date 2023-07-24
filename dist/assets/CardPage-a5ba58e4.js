061495f00a21ba660c62e13201e417c3d65b8221
ibrokhim <ibo-707@mail.ru>
2023-07-24 17:37:05 +0500

    build

---
+++ dist/assets/CardPage-a5ba58e4.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as t,c,d as n}from"./index-a1f6512e.js";const a={};function s(_,d){const e=r("router-view");return t(),c("div",null,[n(e)])}const f=o(a,[["render",s]]);export{f as default};
---
+++ dist/assets/ProccessPage-79555d70.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as c,c as t,d as n}from"./index-a1f6512e.js";const s={};function a(_,l){const e=r("router-view");return c(),t("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
