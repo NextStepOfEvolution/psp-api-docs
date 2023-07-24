2af4cf704d96a2c8fb26eaaec7c9ca391148beab
ibrokhim <ibo-707@mail.ru>
2023-07-24 16:46:04 +0500

    build

--- dist/assets/AdditionalFeaturesPage-f93ea60f.js
+++ dist/assets/AdditionalFeaturesPage-f93ea60f.js
@@ -1,4 +1,4 @@
-import{r as o,o as i,c as r,b as t,u as _,w as n,a as e,d as m}from"./index-8fdf7ed5.js";const d=e("h2",{class:"text-2xl font-bold my-4"},"Дополнительные возможности",-1),h=e("h3",{class:"text-xl font-bold my-4"},"Web-терминал iFrame",-1),p=e("p",{class:"my-4"}," iFrame - это режим работы стандартного Web-терминала, который позволяет отобразить окно Web-терминала внутри одного фрейма на Вашем Web сайте ",-1),u=e("p",{class:"text-xl my-4"},[e("b",null,"Пример вставки iframe кода на странице.")],-1),N=`<!DOCTYPE html>
+import{r as o,o as i,c as r,d as t,u as _,w as n,a as e,e as m}from"./index-40889e75.js";const d=e("h2",{class:"text-2xl font-bold my-4"},"Дополнительные возможности",-1),h=e("h3",{class:"text-xl font-bold my-4"},"Web-терминал iFrame",-1),p=e("p",{class:"my-4"}," iFrame - это режим работы стандартного Web-терминала, который позволяет отобразить окно Web-терминала внутри одного фрейма на Вашем Web сайте ",-1),u=e("p",{class:"text-xl my-4"},[e("b",null,"Пример вставки iframe кода на странице.")],-1),N=`<!DOCTYPE html>
 <html>
     <head>
         <title></title>
---
+++ dist/assets/ProccessPage-8a30f685.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as c,c as t,d as n}from"./index-40889e75.js";const s={};function a(_,l){const e=r("router-view");return c(),t("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
