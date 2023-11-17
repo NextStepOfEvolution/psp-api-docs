cb6326f5652f28c635f70e8089518a6c8dbdde92
ibrokhim <ibo-707@mail.ru>
2023-11-17 17:11:04 +0500

    mobile

--- dist/assets/ProccessPage-ee50cca3.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r,o as c,c as t,d as n}from"./index-40a339be.js";const s={};function a(_,l){const e=r("router-view");return c(),t("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
